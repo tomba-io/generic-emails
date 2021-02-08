084fc1a63d3cdaf9a34f255bedc2baea49a8e8b9
Abedrahim B <abdben966@gmail.com>
2021-02-08 19:44:18 +0100

    feat(): add fucn by hash name

--- src/lib/validation/hash.ts
+++ src/lib/validation/hash.ts
@@ -5,6 +5,7 @@ import error from './../Exception/GenericEmailException'
  * @date: 2020-12-24 20:27:49
  * @link      https://tomba.io
  * @author    Mohamed Ben rebia <b.mohamed@tomba.io>
+ * @author    Abedrahim Ben rebia <b.abedrahim@tomba.io>
  * @license   https://opensource.org/licenses/MIT The MIT License
  */
 
@@ -14,7 +15,7 @@ export default class Hash {
   /**
    * Helper method for checking string if is hash
    * @param _str your string
-   * @param _algorithm algo name ,Algorithm is one of ['md4', 'md5', 'sha1', 'sha256', 'sha384', 'sha512', 'ripemd128', 'ripemd160', 'tiger128', 'tiger160', 'tiger192', 'crc32', 'crc32b']
+   * @param _algorithm algo name ,Algorithm is one of "md4" | "md5" | "sha1" | "sha256" | "ripemd128" | "ripemd160" | "tiger128" | "tiger160" | "tiger192" | "crc32" | "crc32b"
    * @see {@link https://github.com/asaskevich/govalidator/blob/7a23bdc65eef5f3783e782b436f3065eae3fc72d/validator.go#L577/|GitHub}
    *
    * ### example
@@ -40,10 +41,6 @@ export default class Hash {
       len = '48'
     } else if (algo === 'sha256') {
       len = '64'
-    } else if (algo === 'sha384') {
-      len = '96'
-    } else if (algo === 'sha512') {
-      len = '128'
     } else {
       len = false
     }
@@ -60,5 +57,84 @@ export default class Hash {
     } else {
       return false
     }
+  }
+
+  /**
+   * checks is a string is a crc32 hash
+   * @param _str
+   */
+  static IsCrc32(_str: string): boolean {
+    return Hash.IsHash(_str, 'crc32')
+  }
+
+  /**
+   * checks is a string is a crc32b hash
+   * @param _str
+   */
+  static isCrc32b(_str: string): boolean {
+    return Hash.IsHash(_str, 'crc32b')
+  }
+  /**
+   * checks is a string is a md5 hash
+   * @param _str
+   */
+  static isMd5(_str: string): boolean {
+    return Hash.IsHash(_str, 'md5')
+  }
+  /**
+   * checks is a string is a md4 hash
+   * @param _str
+   */
+  static isMd4(_str: string): boolean {
+    return Hash.IsHash(_str, 'md4')
+  }
+  /**
+   * checks is a string is a ripemd128 hash
+   * @param _str
+   */
+  static isRipemd128(_str: string): boolean {
+    return Hash.IsHash(_str, 'ripemd128')
+  }
+  /**
+   * checks is a string is a tiger128 hash
+   * @param _str
+   */
+  static isTiger128(_str: string): boolean {
+    return Hash.IsHash(_str, 'tiger128')
+  }
+  /**
+   * checks is a string is a sha1 hash
+   * @param _str
+   */
+  static isSha1(_str: string): boolean {
+    return Hash.IsHash(_str, 'sha1')
+  }
+  /**
+   * checks is a string is a ripemd160 hash
+   * @param _str
+   */
+  static isRipemd160(_str: string): boolean {
+    return Hash.IsHash(_str, 'ripemd160')
+  }
+  /**
+   * checks is a string is a tiger160 hash
+   * @param _str
+   */
+  static isTiger160(_str: string): boolean {
+    return Hash.IsHash(_str, 'tiger160')
+  }
+  /**
+   * checks is a string is a tiger192 hash
+   * @param _str
+   */
+  static isTiger192(_str: string): boolean {
+    return Hash.IsHash(_str, 'tiger192')
+  }
+  /**
+   * checks is a string is a sha256 hash
+   * @param _str
+   */
+  static isSha256(_str: string): boolean {
+    return Hash.IsHash(_str, 'sha256')
   }
 }
